ed7305d3c7a13efa86c36af46a899ecc81edb35b
Your Name <you@example.com>
2025-02-11 12:15:46 +0100

    client and server in tcp replaced

--- lab1/tcp/client.py
+++ lab1/tcp/client.py
@@ -3,17 +3,15 @@ import socket
 TCP_IP = "127.0.0.1"
 TCP_PORT = 55000
 BUFFER_SIZE = 1024
+msg = "Hello, World!"
 
-sconn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
 
-sconn.bind((TCP_IP, TCP_PORT))
-print(f'binding to {TCP_IP}:{TCP_PORT}')
-sconn.listen(1)
+print('sending data...')
+s.send(msg.encode('ascii'))
 
-s, addr = sconn.accept()
 rawdata = s.recv(BUFFER_SIZE)
-print("received data:", rawdata.decode('ascii'))
-s.send(rawdata)
+print("received data:\n", rawdata.decode('ascii'))
 
 s.close()
-sconn.close()
